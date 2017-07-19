3fda1ae43d24cb029c95c6eeae73031232096847
Dan Murphy <d.p.murph19@gmail.com>
2017-07-19 17:47:21 -0600

    validation stuff

--- register/serializers.py
+++ register/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
+from django.contrib.auth import password_validation
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+
+    def validate_password(self, value):
+        password_validation.validate_password(value, self.instance)
+        return value
 
     def create(self, validated_data):
-
         user = User.objects.create(
             username=validated_data['username'],
             email=validated_data['email']
@@ -17,3 +22,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password', 'email')
+        write_only_fields = ('password',)
