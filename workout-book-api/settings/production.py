0ef7c67f003f235cdec0aca12ec15ba0f84cfc9f
Dan Murphy <d.p.murph19@gmail.com>
2017-09-12 17:34:07 -0600

    some production settings updates

--- workout-book-api/settings/production.py
+++ workout-book-api/settings/production.py
@@ -26,3 +26,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ADMINS = (
     ('Daniel Murphy', 'd.p.murph19@gmail.com'),
 )
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE¶ = True
